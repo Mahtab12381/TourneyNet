0f65f1ee5a70577900214db01315030090ab3544
Mahtab12381 <mahtab.sani12381@gmail.com>
2023-04-30 16:33:36 +0600

    participents added

---
+++ DAL/Model/Participants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class Participants
+    {
+        [Key]
+        public int participant_id { get; set; }
+        public string team_name { get; set; }
+
+        public string team_logo { get; set;}
+
+    }
+}
