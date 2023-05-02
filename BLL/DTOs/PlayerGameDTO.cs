9ea19bb415082f16c49a9847832c0a51a9b06594
Mridul Roy <77848289+mridul-roy@users.noreply.github.com>
2023-05-02 18:36:50 +0600

    PlayerGame Added

---
+++ BLL/DTOs/PlayerGameDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class PlayerGameDTO
+    {
+        public int Player_Game_Id { get; set; }
+        
+        public string Game_Name { get; set; }
+
+        public string Player_Skill { get; set; }
+    }
+}
